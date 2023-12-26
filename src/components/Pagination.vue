3bb3b310aee567d73f5fa6ff1b6654a60ee9b029
BM_BigMouse <2692721469@qq.com>
2023-12-26 15:18:57 +0800

    dev

---
+++ src/components/Pagination.vue
@@ -0,0 +1,17 @@
+<template>
+    <el-pagination background layout="prev, pager, next" :total="1000">
+    </el-pagination>
+</template>
+
+<script>
+export default {
+    data() {
+        return {};
+    },
+    methods: {
+    }
+}
+</script>
+
+
+<style></style>
